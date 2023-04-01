f8d70b4ba7231fc5220fb6542456621706a77144
Christan Gerloff <christian.gerloff@rwth-aachen.de>
2023-04-01 18:21:34 +0800

    adj acm test pbar #25

--- tests/unit/test_acm_searcher.py
+++ tests/unit/test_acm_searcher.py
@@ -1,34 +1,57 @@
 import datetime
-import pytest
 from urllib.parse import quote_plus
-import findpapers.searchers.acm_searcher as acm_searcher
-from findpapers.models.search import Search
+import pytest
+from tqdm import tqdm
+
 from findpapers.models.publication import Publication
+from findpapers.models.search import Search
+from findpapers.searchers.acm_searcher import (
+    _get_paper,
+    _get_paper_metadata,
+    _get_paper_page,
+    _get_result,
+    _get_search_url,
+    run,
+)
+
+
+@pytest.fixture
+def test_pbar():
+    """Fixture for creating a progress bar object."""
+    return tqdm(desc='Progress', total=20)
 
 
 def test_get_search_url(search: Search):
-
-    url = acm_searcher._get_search_url(search)
-
+    """Test the get_search_url function."""
+    url = _get_search_url(search)
     query = search.query.replace(' AND NOT ', ' NOT ')
-
+    expected_url = 'https://dl.acm.org/action/doSearch?'
+    assert url.startswith(expected_url)
     assert quote_plus(query) in url
-    assert url.startswith('https://dl.acm.org/action/doSearch?')
 
 
-def test_mocks():
+def test_get_paper_page():
+    """Test the get_paper_page function by mock."""
+    paper_page = _get_paper_page()
+    assert paper_page is not None
 
-    assert acm_searcher._get_result() is not None
-    assert acm_searcher._get_paper_page() is not None
-    assert acm_searcher._get_paper_metadata() is not None
+
+def test_get_paper_metadata():
+    """Test the get_paper_metadata function by mock."""
+    metadata = _get_paper_metadata()
+    assert metadata is not None
+
+    
+def test_get_result():
+    """Test the get_result function by mock."""
+    metadata = _get_result()
+    assert metadata is not None
 
 
 def test_get_paper():
-
-    paper_page = acm_searcher._get_paper_page()
-
-    paper = acm_searcher._get_paper(paper_page, 'fake-paper-doi', 'fake-url')
-
+    """Test the get_paper function."""
+    paper_page = _get_paper_page()
+    paper = _get_paper(paper_page, 'fake-paper-doi', 'fake-url')
     assert paper is not None
     assert paper.title is not None
     assert paper.doi is not None
@@ -43,19 +66,17 @@ def test_get_paper():
 
 @pytest.mark.skip(reason="It needs some revision after some tool's refactoring")
 def test_run(search: Search):
-
+    """Test the run function."""
     search.limit = 14
     search.limit_per_database = None
-
-    acm_searcher.run(search)
-
+    run(search)
     assert len(search.papers) == 14
 
-def test_run_without_pbar(search: Search, pbar=None):
 
+@pytest.mark.parametrize("pbar", [None, pytest.lazy_fixture("test_pbar")])
+def test_run_with_or_without_pbar(search: Search, pbar):
+    """Test the run function with or without a progress bar."""
     search.limit = 10
     search.limit_per_database = None
-
-    acm_searcher.run(search, pbar)
-
+    run(search, pbar=pbar)
     assert len(search.papers) == 10
